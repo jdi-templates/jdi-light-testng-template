c6525eca1fee269f1b9ba165658d5c364628c11e
Supreme85 <romanyister@gmail.com>
2018-11-06 19:30:30 +0300

    update template to version 1.0.14

--- src/main/java/org/mytests/uiobjects/example/site/pages/JDIPerformancePage.java
+++ src/main/java/org/mytests/uiobjects/example/site/pages/JDIPerformancePage.java
@@ -1,46 +1,19 @@
 package org.mytests.uiobjects.example.site.pages;
 
-import com.epam.jdi.light.elements.base.UIElement;
-import com.epam.jdi.light.elements.complex.WebList;
+import com.epam.jdi.light.elements.complex.table.FastTable;
 import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
-import com.epam.jdi.light.elements.pageobjects.annotations.JPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.simple.Css;
-import com.epam.jdi.light.elements.pageobjects.annotations.simple.XPath;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import com.epam.jdi.light.elements.pageobjects.annotations.Url;
+import com.epam.jdi.light.ui.html.common.TextArea;
+import com.epam.jdi.light.ui.html.complex.Dropdown;
 
-import java.util.List;
-
-@JPage(url = "/performance.html")
+@Url("/performance.html")
 public class JDIPerformancePage extends WebPage {
-    //Table
-    @Css("#users-table") public WebElement table;
+    // @Css("#users-table")
+    public static FastTable usersTable;
 
-    public WebElement getUser(String name, String email) {
-        List<WebElement> rows = table.findElements(By.tagName("tr"));
-        for (WebElement row : rows) {
-            List<WebElement> columns = row.findElements(By.tagName("td"));
-            if (columns.size() == 4 && columns.get(0).getText().contains(name) &&
-                columns.get(2).getText().contains(email))
-                return row;
-        }
-        return null;
-    }
+    // @Css("#user-names")
+    public static Dropdown userNames;
 
-    // Dropdown
-    @Css("#user-names option")
-    public WebList names;
-    @XPath("//*[@id='user-names']//*[text()='%s']")
-    public UIElement namesDropdown;
-    @Css("select[ui=dropdown]") WebElement dropdownValue;
-    @Css("#user-names option") List<WebElement> dropdown;
-
-    public String isSelected() {
-        return dropdownValue.getText();
-    }
-
-    // Textfield
-    @Css("#textarea-performance")
-    public UIElement textfield;
+    //@Css("#textarea-performance")
+    public static TextArea textareaPerformance;
 }
--- src/test/java/org/mytests/tests/SimpleTestsInit.java
+++ src/test/java/org/mytests/tests/SimpleTestsInit.java
@@ -1,4 +1,4 @@
-package org.mytests.tests.example;
+package org.mytests.tests;
 
 import com.epam.jdi.light.driver.WebDriverFactory;
 import org.mytests.uiobjects.example.site.SiteJdi;
@@ -7,14 +7,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
 import static com.epam.jdi.light.actions.ActionHelper.ERROR_THROWN;
-import static com.epam.jdi.light.elements.init.PageFactory.initElements;
-import static com.epam.jdi.light.logger.LogLevels.STEP;
 import static com.epam.jdi.light.settings.WebSettings.logger;
+import static com.epam.jdi.light.ui.html.PageFactory.initElements;
+import static org.mytests.uiobjects.example.site.SiteJdi.homePage;
 
 public class SimpleTestsInit {
     @BeforeSuite(alwaysRun = true)
     public static void setUp() {
         initElements(SiteJdi.class);
+        homePage.open();
         logger.info("Run Tests");
     }
 
