5239924982f0700fba0e9b82886d7e078de281ad
Supreme85 <romanyister@gmail.com>
2018-11-12 22:21:10 +0300

    update template tests to jdi-html 1.0.16

--- src/test/java/org/mytests/tests/example/JDIPerformanceTests.java
+++ src/test/java/org/mytests/tests/example/JDIPerformanceTests.java
@@ -7,11 +7,13 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static com.epam.jdi.light.elements.complex.table.Column.inColumn;
 import static com.epam.jdi.light.elements.complex.table.TableMatchers.containsValue;
 import static org.mytests.tests.preconditions.Preconditions.shouldBeLoggedIn;
 import static org.mytests.uiobjects.example.TestData.TABLE_SNAPSHOOT;
-import static org.mytests.uiobjects.example.entities.Defaults.DEFAULT_CONTACTS;
+import static org.mytests.uiobjects.example.entities.Defaults.DEFAULT_CONTACT;
 import static org.mytests.uiobjects.example.site.SiteJdi.contactFormPage;
 import static org.mytests.uiobjects.example.site.SiteJdi.performancePage;
 import static org.mytests.uiobjects.example.site.pages.ContactFormPage.contactForm;
@@ -28,21 +30,21 @@ public class JDIPerformanceTests extends SimpleTestsInit {
     @Test
     public void fillContactFormTest() {
         contactFormPage.shouldBeOpened();
-        contactForm.submit(DEFAULT_CONTACTS);
-        contactForm.check(DEFAULT_CONTACTS);
+        contactForm.submit(DEFAULT_CONTACT);
+        contactForm.check(DEFAULT_CONTACT);
     }
 
     @Test
     public void hugeTableSearchTest() {
         StopWatch timer = StopWatch.createStarted();
-        /*performancePage.table.assertThat().hasRowsWhereValue(
-                $(containsString("Meyer"), inColumn("Name")),
-                $(containsString("co.uk"), inColumn("Email")));
+        /*usersTable.assertThat().hasRowWithValues(
+            containsValue("Meyer", inColumn("Name")),
+            containsValue("co.uk", inColumn("Email")));
         */
         Line row = usersTable.row(
             containsValue("Meyer", inColumn("Name")),
             containsValue("co.uk", inColumn("Email")));
-        System.out.println("Huge table search test Time: " + timer.getTime());
+        System.out.println("Huge table search test Time: " + timer.getTime() + "ms");
         Assert.assertEquals(row.getValue(),
         "Brian Meyer,(016977) 0358,mollis.nec@seddictumeleifend.co.uk,Houston");
     }
@@ -51,7 +53,7 @@ public class JDIPerformanceTests extends SimpleTestsInit {
     public void hugeTableValidateTest() {
         StopWatch timer = StopWatch.createStarted();
         String actualTable = usersTable.preview();
-        System.out.println("Huge table validate test Time: " + timer.getTime());
+        System.out.println("Huge table validate test Time: " + timer.getTime() + "ms");
         Assert.assertEquals(actualTable, TABLE_SNAPSHOOT);
     }
 
@@ -60,7 +62,7 @@ public class JDIPerformanceTests extends SimpleTestsInit {
         String name = "Charles Byers";
         StopWatch timer = StopWatch.createStarted();
         userNames.select(name);
-        System.out.println("Big dropdown test Time: " + timer.getTime());
+        System.out.println("Big dropdown test Time: " + timer.getTime() + "ms");
         Assert.assertEquals(userNames.selected(), name);
     }
 
@@ -73,6 +75,6 @@ public class JDIPerformanceTests extends SimpleTestsInit {
                 "Usu esse utroque sapientem ad. Eam ut consul soleat sapientem, cu dolor consequuntur vis. Erat temporibus mea id, has ex dicam tritani. Pertinacia expetendis consectetuer eos ei, vidit malis periculis est ea, ne nam movet fuisset. Pro id habemus definitiones, in ferri solum reprehendunt mei. Vel eligendi honestatis liberavisse id.";
         StopWatch timer = StopWatch.createStarted();
         textareaPerformance.setText(text + "\\n"+ text);
-        System.out.println("Long text test Time: " + timer.getTime());
+        System.out.println("Long text test Time: " + timer.getTime() + "ms");
     }
 }
