997f1c8c513baa01c63abedf4837282bb8d8e8a9
Supreme85 <romanyister@gmail.com>
2019-01-14 14:13:46 +0300

    add retry listner

--- src/test/java/org/mytests/tests/example/MenuTests.java
+++ src/test/java/org/mytests/tests/example/MenuTests.java
@@ -6,10 +6,11 @@ import org.testng.annotations.Test;
 
 import static org.mytests.uiobjects.example.entities.LeftMenuData.*;
 import static org.mytests.uiobjects.example.site.SiteJdi.*;
+import static org.testng.Assert.assertTrue;
 
 public class MenuTests extends SimpleTestsInit {
 
-    @Test
+    /*@Test
     public void menuTest() {
         Preconditions.shouldBeLoggedIn();
         homePage.shouldBeOpened();
@@ -17,5 +18,17 @@ public class MenuTests extends SimpleTestsInit {
         datesPage.checkOpened();
         leftMenu.select(ElementsPacks, HTML5);
         html5Page.checkOpened();
+    }*/
+    int count = 0;
+    @Test
+    public void failedTest1() {
+        System.out.println("Retry1: " + count);
+        if (count++ % 2 == 0)
+            assertTrue(false);
+    }
+    @Test
+    public void failedTest2() {
+        System.out.println("Retry2: " + count);
+        assertTrue(false);
     }
 }
